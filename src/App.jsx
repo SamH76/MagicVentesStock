34b9d8604eb9bf0d93826e109c698949fca1c94e
Samantha Henry <henrysamamntha.pro@gmail.com>
2026-04-16 00:31:15 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import background from "./assets/image_accueil.png"; // ton image de fond
+
+function App() {
+  const [pseudo, setPseudo] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Connexion avec pseudo: ${pseudo}, mot de passe: ${password}`);
+  };
+
+  return (
+    <div className="login-page" style={{ backgroundImage: `url(${background})` }}>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Pseudo"
+          value={pseudo}
+          onChange={(e) => setPseudo(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Mot de Passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">S’identifier</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
